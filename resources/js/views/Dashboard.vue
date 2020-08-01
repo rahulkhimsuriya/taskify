33807e5a33329ffe07bfa6079ea20d726204d089
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-08-01 18:43:01 +0530

    display image placeholder

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -8,9 +8,19 @@
         <div
             class="w-1/2 bg-white absolute right-0 inset-y-0 p-10 m-4 rounded shadow"
         >
-            <header>
-                <div></div>
-            </header>
+            <div class="flex flex-col items-center justify-center h-full">
+                <img
+                    src="/image/task_list.svg"
+                    class="h-64 w-64 opacity-75"
+                    alt="Task List"
+                />
+
+                <h1
+                    class="text-3xl font-light text-gray-500 leading-none tracking-wide"
+                >
+                    Manage your work.
+                </h1>
+            </div>
         </div>
     </div>
 </template>
